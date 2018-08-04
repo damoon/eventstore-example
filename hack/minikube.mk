c9977fc0ae3d8c619a58536e9ab28e22b5fe18ef
David Sauer <davedamoon@gmail.com>
2018-08-04 14:56:45 +0200

    added namespace setup

--- hack/minikube.mk
+++ hack/minikube.mk
@@ -1,10 +1,20 @@
 
-.PHONY: minikube-start
-minikube-start: ##@minikube start minikube
-	sudo CHANGE_MINIKUBE_NONE_USER=true minikube start --vm-driver=none
+
+
+.PHONY: minikube-setup
+minikube-setup: minikube-start set-namespace ##@minikube start minikube
 	sudo minikube addons enable freshpod
 	sudo minikube addons enable ingress
 	sudo minikube addons enable heapster
+
+.PHONY: minikube-start
+minikube-start: ##@minikube start minikube
+	sudo CHANGE_MINIKUBE_NONE_USER=true minikube start --vm-driver=none
+
+.PHONY: set-namespace
+set-namespace: ##@minikube set the namespace
+	kubectl apply -f environment/namespace.yaml
+	kubectl config set-context minikube --namespace=eventstore-example
 
 .PHONY: minikube-stop
 minikube-stop: ##@minikube stop minikube
